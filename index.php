088732e37e78b37c5b92c7e784eadff9efbca03a
SamueleLettieri <samuele2073@gmail.com>
2022-11-09 11:25:21 +0100

    creazione form

--- index.php
+++ index.php
@@ -10,12 +10,33 @@
 </head>
 
 <body>
-    <header class="ms_p30">
-        <div class=""><img class="ms_image" src="https://edusogno.com/logo-black.svg" alt=""></div>
+    <header class="ms_p30 ms_background-white">
+        <div class=""><img class="ms_image" src="./assets/img/logo-black.svg" alt=""></div>
     </header>
 
-    <main class="ms_mt">
-        <div class="ms_box"></div>
+    <main class="ms_mt110">
+        <div class="ms_container ms_pt100">
+            <div class="ms_tc">
+                <h1>Hai già un account?</h1>
+
+                <div class="ms_box ms_p30 ms_background-white">
+                    <form action="">
+                        <div class="ms_m40">
+                            <label for="">Inserisci l'e-mail</label>
+                            <input type="email" placeholder="name@example.com">
+                            <small></small>
+                        </div>
+                        <div class="ms_m40">
+                            <label for="">Inserisci la password</label>
+                            <input type="password" placeholder="Scrivi qui">
+                            <small></small>
+                        </div>
+                        <button class="ms_m20">ACCEDI</button>
+                    </form>
+                    <small class="">Non hai ancora un profilo? <a href="#"><strong>Registrati</strong></a></small>
+                </div>
+            </div>
+        </div>
     </main>
 </body>
 
