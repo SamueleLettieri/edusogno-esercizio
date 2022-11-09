9a693c2225742d89bc77d10f03596722d517949f
SamueleLettieri <samuele2073@gmail.com>
2022-11-09 10:20:09 +0100

    header completato

--- index.php
+++ index.php
@@ -5,11 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/style.css">
     <title>Edusogno</title>
 </head>
 
 <body>
+    <header class="ms_p30">
+        <div class=""><img class="ms_image" src="https://edusogno.com/logo-black.svg" alt=""></div>
+    </header>
 
+    <main class="ms_mt">
+        <div class="ms_box"></div>
+    </main>
 </body>
 
 </html>
